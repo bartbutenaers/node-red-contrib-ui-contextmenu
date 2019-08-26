f4058449f94bb2b254c6d4e325042e9e47e4b34e
Mclaughlin <UK07490@nissaneu.nmcorp.nissan.biz>
2019-08-26 17:45:50 +0100

    jquery context menu

--- context_menu.js
+++ context_menu.js
@@ -24,7 +24,8 @@ module.exports = function(RED) {
         // Based on the nice tutorial of Richard Umoffia (see https://dev.to/iamafro/how-to-create-a-custom-context-menu--5d7p)
         // TODO font size doesn't work
         var html = String.raw`
-        	<style>
+            <style>
+                .menu-icon { width: 120px; }
                 .menu {
                     width: 120px;
                     box-shadow: 0 4px 5px 3px rgba(0, 0, 0, 0.2);
@@ -48,9 +49,9 @@ module.exports = function(RED) {
                     }
                 }
             </style>
+            <span id='span_'` + config.id + `' />
             <div id='div_` + config.id + `' ng-init='init(` + configAsJson + `)' class="menu" display='none';>
-                <ul class="menu-options">
-                </ul>
+                
             </div>
         `;
                         
@@ -123,53 +124,112 @@ module.exports = function(RED) {
                     initController: function($scope, events) {
                         // Remark: all client-side functions should be added here!  
                         // If added above, it will be server-side functions which are not available at the client-side ...
-                        
-                        // Fill the menu with menu items
-                        function setupMenu(menuItems) {
-                            // Make sure there are no previous menu options available
-                            $scope.menuList.innerHTML = "";;
-                        
-                            menuItems.forEach(function(menuItem) {
-                                var optionElement = document.createElement('option');
-                                
-                                optionElement.value = menuItem.command || "undefined";
-                                optionElement.class = "menu-option";
-                                
-                                if (menuItem.enabled) {
-                                    optionElement.classList.remove("disabled");
-                                }
-                                else {
-                                    optionElement.classList.remove("disabled");
-                                }
+                        console.log("ui_context_menu: ui_contextmenu.initController()")
+                        function getPosition(scope, msg){
+                            var xp = "0px",yp = "0px";
+                            if (scope.config.position === "msg" && msg && msg.position) {
+                                // Position the context menu based on the coordinates in the message
+                                xp = msg.position.x + scope.unit;
+                                yp  = msg.position.y + scope.unit;
+                            }
+                            else {
+                                // Position the context menu based on the coordinates in the config screen
+                                xp = scope.config.xCoordinate + scope.unit;
+                                yp  = scope.config.yCoordinate + scope.unit;
+                            }
+                            return {left: xp, top: yp};
+                        }
 
-                                if (menuItem.visible) {
-                                    optionElement.style.display = "block";
-                                }
-                                else {
-                                    optionElement.style.display = "none";
-                                }                                
-                                
-                                if (menuItem.icon && menuItem.icon != "") {
-                                    optionElement.innerHTML = '<i class="fa fa-heart"></i>' + menuItem.label;
-                                }
-                                else {
-                                    optionElement.innerHTML = menuItem.label;
-                                } 
-                                
-                                optionElement.addEventListener('click', function() {
-                                    node.send({payload: command});
-                                });
-                                
-                                $scope.menuList.appendChild(optionElement);
+                        //https://stackoverflow.com/questions/14919894/getscript-but-for-stylesheets-in-jquery
+                        function loadJavascriptAndCssFiles(urls, successCallback, failureCallback) {
+
+                            $.when.apply($,
+                                $.map(urls, function(url) {
+                                    if(url.endsWith(".css")) {
+                                        return $.get(url, function(css) {
+                                            $("<style>" + css + "</style>").appendTo("head");
+                                        });
+                                    } else {
+                                        return $.getScript(url);
+                                    }
+                                })
+                            ).then(function() {
+                                if (typeof successCallback === 'function') successCallback();
+                            }).fail(function() {
+                                if (typeof failureCallback === 'function') failureCallback();
                             });
+                        
+                        }
+                        function createMenu(scope, selector, callback){
+                            try {
+                                $.contextMenu("destroy",selector);
+                            } catch (error) {
+                            }
+
+                            // Make sure there are no previous menu options available
+                            var items = {};
+                            console.log("createMenu")
+                            scope.config.menuItems.forEach(function(menuItem) {
+
+                                items[menuItem.label] = {
+                                    name: menuItem.label,
+                                    icon: menuItem.icon,
+                                    disabled: menuItem.disabled,
+                                    // callback: function(itemKey,opt,e){
+                                    //     scope.send({payload: this, opt: opt, itemKey: itemKey});
+                                    // }
+                                }
+                                
+                            });
+
+                            var menuConfig = {
+                                callback: function(key, options) {
+                                    scope.send({payload: key});
+                                    $(scope.menuDivId).contextMenu("hide");
+                                },
+                                selector: selector,
+                                trigger: "none",
+                                position: function(opt, x, y){
+                                    opt.$menu.css({top: y, left: x, position: "absolute"});
+                                },
+                                items: items
+                            };
+
+                            if($.contextMenu){
+                                $.contextMenu(menuConfig);
+                                callback();
+                                return;
+                            } else {
+                                var urls = ['/ui_context_menu/lib/jquery.contextMenu.min.js', '/ui_context_menu/lib/jquery.contextMenu.min.css']
+                                loadJavascriptAndCssFiles(urls, 
+                                    function(){
+                                        //success
+                                        console.log("ui_context_menu: JQuery plugin loaded");
+                                        $.contextMenu(menuConfig);
+                                        callback();
+                                    },
+                                    function(){
+                                        //fail
+                                    });
+
+
+                                // Load the JQuery plugin for a contectMenu (https://github.com/lai32290/TableHeadFixer).
+                                // See explantation about third party Js libraries in Node-Red on https://groups.google.com/d/msg/node-red/AgLNx0PpuBA/ZBfKYZ9GBAAJ                                
+                                // $.getScript('/ui_context_menu/lib/jquery.contextMenu.min.js').done(function(data, textStatus, jqxhr) {
+                                //     console.log("ui_context_menu: JQuery plugin loaded");
+                                //     $.contextMenu(menuConfig);
+                                //     callback();
+                                // });   
+                            }
+                            //scope.menuList.appendChild(optionElement);
                         } 
                             
                         $scope.flag = true;
 
                         $scope.init = function (config) {
+                            console.log("ui_context_menu: ui_contextmenu.initController > $scope.init()")
                             $scope.config   = config;
-                            $scope.menuDiv  = document.getElementById("div_" + config.id);
-                            $scope.menuList = $scope.menuDiv.querySelector('ul');
+
                             
                             if ($scope.config.unit === "perc") {
                                 $scope.unit = "%";
@@ -178,41 +238,69 @@ module.exports = function(RED) {
                                 $scope.unit = "px";
                             }
                             
-                            // When menu items available in the config screen, add those to the menu already
-                            if ($scope.config.menuItems) {
-                                setupMenu($scope.config.menuItems);
-                            }
+                            $scope.menuDivId = "body > md-content"; //workaround!
+
+                            //THIS SHOULD WORK - but adding aconectMenu to dynamically added element 
+                            //results in an error client side
+                            //instead, I had to create it every time and attach it to a static element
+                            //and since there may be more than one contextMenu, I have to destry and re-create!!!
+                            // // When menu items available in the config screen, add those to the menu already
+                            // $scope.menuDivId = "menu_" + config.id;
+                            // var menuDiv = document.createElement('div');               
+                            // menuDiv.id = $scope.menuDivId;
+                            // menuDiv.style.cssText = 'position:absolute';
+                            // document.body.appendChild(menuDiv);                            
+                            // if ($scope.config.menuItems) {
+                            //     var pos = getPosition($scope);
+                            //     createMenu($scope, $scope.config.menuItems, $scope.menuDivId, pos.left, pos.top, function(){
+                            //     });
+                            // }
                         }
 
                         $scope.$watch('msg', function(msg) {
+                            console.log("ui_context_menu: ui_contextmenu.initController > $scope.$watch('msg'...)")
+
                             // Ignore undefined messages.
                             if (!msg) {
                                 return;
                             }
-                            
-                            debugger;
-                            
-                            // TODO msg should also be able to disable or hide menu items from $scope.config.menuItems
+                            if (!$scope.config) {
+                                console.log("ui_context_menu: $scope.config is empty :(")
+                                return;
+                            }
+                                        
+                            var pos = getPosition($scope, msg);
 
-                            if ($scope.config.position === "msg" && msg.position) {
-                                // Position the context menu based on the coordinates in the message
-                                $scope.menuDiv.style.left = msg.position.x + $scope.unit;
-                                $scope.menuDiv.style.top  = msg.position.y + $scope.unit;
-                            }
-                            else {
-                                // Position the context menu based on the coordinates in the config screen
-                                $scope.menuDiv.style.left = $scope.config.xCoordinate + $scope.unit;
-                                $scope.menuDiv.style.top  = $scope.config.yCoordinate + $scope.unit;
-                            }
-            
                             if ($scope.config.menu === "msg" && msg.menu) {
                                 // Show the menu items that have been specified in the input message
-                                setupMenu(msg.menu);
-                            }
+                                createMenu($scope, $scope.menuDivId, function(){
+                                    //hack city:  set parent div h/w 0 & align to top/left 0 :) 
+                                    $scope.menuDiv.parentElement.style.width = "0px";
+                                    $scope.menuDiv.parentElement.style.height = "0px";
+                                    $scope.menuDiv.parentElement.style.left = "0px";
+                                    $scope.menuDiv.parentElement.style.top = "0px";
+                                    $scope.menuDiv.parentElement.style.zIndex = "9999";
+                                    $($scope.menuDiv.parentElement).removeClass("nr-dashboard-template");
+                                    $scope.menuDiv.parentElement.style.display = "";//show it
+                                    // Show the menu to the user
+                                    $($scope.menuDivId).contextMenu({x:pos.left,y:pos.top}); 
+                                })                       
+                            } else {
                             
-                            // Show the menu to the user
-                            $scope.menuDiv.style.display = "block";
-                        });
+                                //adding a contextMenu to dynamically added element 
+                                //results in an error client side
+                                //instead, I had to create it every time and attach it to a static element
+                                //and since there may be more than one contextMenu, I have to destry and re-create!!!
+                                // // When menu items available in the config screen, add those to the menu already
+                                if ($scope.config.menuItems) {
+                                    var pos = getPosition($scope);
+                                    createMenu($scope, $scope.menuDivId, function(){
+                                        $($scope.menuDivId).contextMenu({x:pos.left,y:pos.top}); 
+                                    });
+                                }
+                                                                
+                            }
+                        });                        
                     }
                 });
             }
@@ -229,4 +317,16 @@ module.exports = function(RED) {
     }
 
     RED.nodes.registerType("ui_context_menu", ContextMenuNode);
+
+        // Make all the static resources from this node public available (i.e. third party JQuery plugin tableHeadFixer.js).
+    // TODO is dit nodig?  of gewoon een script file includen op de html
+    RED.httpAdmin.get('/ui_context_menu/*', function(req, res){
+        var options = {
+            root: __dirname /*+ '/static/'*/,
+            dotfiles: 'deny'
+        };
+       
+        // Send the requested file to the client (in this case it will be tableHeadFixer.js)
+        res.sendFile(req.params[0], options)
+    });
 }
